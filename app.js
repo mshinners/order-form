ba3781876487883bf7d3588f9e855d050099922d
Nathan Moore <ncmooore1986@gmail.com>
2017-08-24 16:04:47 -0700

    work in progress

---
+++ app.js
@@ -0,0 +1,68 @@
+'use strict';
+
+var names = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dogduck', 'dragon', 'pen', 'petsweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'watercan', 'wineglass'];
+
+var paths = ['img/bag.jpg', 'img/banana.jpg', 'img/bathroom.jpg', 'img/boots.jpg', 'img/breakfast.jpg', 'img/bubblegum.jpg', 'img/chair.jpg', 'img/cthulhu.jpg', 'img/dogduck.jpg', 'img/dragon.jpg', 'img/pen.jpg', 'img/petsweep.jpg', 'img/scissors.jpg', 'img/shark.jpg', 'img/sweep.png', 'img/tauntaun.jpg', 'img/unicorn.jpg', 'img/usb.gif', 'img/watercan.jpg', 'img/wineglass.jpg'];
+
+var savedOrders = [];
+
+function Product(name, paths) {
+  this.name = name;
+  this.paths = paths;
+}
+
+function createDropDownMenu() {
+  var table = document.getElementById('productInformation');
+  var tr = document.createElement('tr');
+  table.appendChild(tr);
+  var td = document.createElement('td');
+  td.setAttribute('id', 'whichProduct');
+  tr.appendChild(td);
+  td = document.createElement('td');
+  td.setAttribute('id', 'counter');
+  tr.appendChild(td);
+  td = document.getElementById('whichProduct');
+  var select = document.createElement('select');
+  td.appendChild(select);
+  for (var i = 0; i < names.length; i++) {
+    var option = document.createElement('option');
+    option.setAttribute('id', 'product');
+    option.innerText = names[i];
+    select.appendChild(option);
+  }
+  td = document.createElement('td');
+  tr.appendChild(td);
+  var input = document.createElement('input');
+  input.setAttribute('id', 'quantity');
+  input.setAttribute('type', 'number');
+  input.setAttribute('placeholder', 'Quantity');
+  input.setAttribute('value', '1');
+  td.appendChild(input);
+}
+
+var submit = document.getElementById('form');
+submit.addEventListener('submit', handleSubmit);
+
+function Order(custName, custPhone, custAddress, custCity, custState, custZip, custProduct, custQuantity, custCredit, custExp, custCsv) {
+  this.custName = custName;
+  this.custPhone = custPhone;
+  this.custAddress = custAddress;
+  this.custCity = custCity;
+  this.custState = custState;
+  this.custZip = custZip;
+  this.custProduct = custProduct;
+  this.custQuanity = custQuanity;
+  this.custCredit = custCredit;
+  this.custExp = custExp;
+  this.custCsv = custCsv;
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  var newOrder = new Order(event.target.name.value, event.target.phone.value, event.target.address.value, event.target.city.value, event.target.state.value, event.target.zip.value, event.target.product.value, event.target.quantity.value, event.target.ccNum.value, event.target.expDate.value, event.target.csv.value);
+  savedOrders.push(newOrder);
+  var dataToSave = JSON.stringify(savedOrders);
+  localStorage.setItem('submittedOrders', dataToSave);
+}
+
+createDropDownMenu();
